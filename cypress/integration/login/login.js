110be020b651037923b80c18c61e234edc460b28
Yesid <yesid.lopez@correo.icesi.edu.co>
2020-09-01 08:33:39 -0500

    skeleton

---
+++ cypress/integration/login/login.js
@@ -0,0 +1,12 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+const url = "http://automationpractice.com/";
+
+Given("", () => {
+});
+
+When("", () => {
+});
+
+Then("", () => {
+});
